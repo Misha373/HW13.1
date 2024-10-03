f9e48dd7189f6264e811d9e89aa28ba691c34408
Misha373 <misasapka1@gmail.com>
2024-10-03 22:32:48 +0300

    project is are rady

---
+++ HW13.1/DoTasks.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW13._1
+{
+    internal class DoTasks
+    {
+        public List<string> Tasks = new List<string>();
+        public List<bool> TasksStatus = new List<bool>();
+        public void AddTasks()
+        {
+            Console.Write("Введіть назву справи: ");
+            string task = Console.ReadLine();
+            Tasks.Add(task);
+            TasksStatus.Add(false); // Нові справи спочатку позначаються як невиконані
+            Console.WriteLine("Справу додано до списку.");
+        }
+        public void DisplayTasks()
+        {
+            if (Tasks.Count == 0)
+            {
+                Console.WriteLine("Список справ порожній.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Список справ:");
+                for (int i = 0; i < Tasks.Count; i++)
+                {
+                    string status;
+                    if (TasksStatus[i])
+                    {
+                        status = "[Виконано]";
+                    }
+                    else
+                    {
+                        status = "[Не виконано]";
+
+                    }
+                    
+                    Console.WriteLine($"{i + 1}. {Tasks[i]} {status}");
+                }
+            }
+        }
+        public void MarkTaskAsDone()
+        {
+            if (Tasks.Count == 0)
+            {
+                Console.WriteLine("Список справ порожній.");
+                return;
+            }
+            try
+            {
+                Console.Write("Введіть номер справи яку ви виконали: ");
+                int Num = int.Parse(Console.ReadLine());
+               TasksStatus[Num-1] = true;
+                Console.WriteLine("Справа успішно виконана.");
+            }
+            catch
+            {
+                Console.WriteLine("Невірний номер справи");
+            }
+        }
+        public void DeleteTask()
+        {
+            if(Tasks.Count == 0)
+            {
+                Console.WriteLine("Список справ порожній.");
+                return;
+            }
+            try
+            {
+
+
+                Console.Write("Введіть номер справи яку ви хочете видалити: ");
+                int Num = int.Parse(Console.ReadLine());
+                Tasks.RemoveAt(Num-1);
+                TasksStatus.RemoveAt(Num-1);
+                Console.WriteLine("Справа успішно видалена");
+            }
+            catch
+            {
+                Console.WriteLine("Невірний номер справи");
+            }
+        }
+
+    }
+}
--- HW13.1/Program.cs
+++ HW13.1/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace HW13._1
+﻿using System.Threading.Tasks;
+
+namespace HW13._1
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            DoTasks dotasks = new DoTasks();
+            Program program = new Program();
             
+            while (true)
+            {
+                Console.WriteLine("\nМеню:");
+                Console.WriteLine("1. Додати нову справу");
+                Console.WriteLine("2. Вивести список справ");
+                Console.WriteLine("3. Позначити справу як виконану");
+                Console.WriteLine("4. Видалити справу");
+                Console.WriteLine("5. Вийти з програми");
+                Console.Write("Введіть ваш вибір: ");
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        dotasks.AddTasks();
+                        break;
+                    case "2":
+                        dotasks.DisplayTasks();
+                        break;
+                    case "3":
+                        dotasks.MarkTaskAsDone();
+                        break;
+                    case "4":
+                        dotasks.DeleteTask();
+                        break;
+                    case "5":
+                        Console.WriteLine("Завершення роботи програми.");
+                        return;
+                    default:
+                        Console.WriteLine("Невірний вибір. Спробуйте ще раз.");
+                        break;
+                }
+                
+            }
         }
     }
 }
